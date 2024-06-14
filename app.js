0ebcb82b2f18bcd0fb871d34b9f3db663adeba13
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-06-14 20:53:46 +0300

    creating base layout

--- app.js
+++ app.js
@@ -68,8 +68,12 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Olya',
+  });
 });
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
