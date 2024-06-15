41231b601e5d6db026307c84d72e78f917438e21
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-06-15 08:39:43 +0300

    building tour overview page'

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,16 @@
-exports.getOverview = (req, res) => {
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) get tour data from collection
+  const tours = await Tour.find();
+
+  //2) Build temolate
+  // 3) render template using the date from step 1
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker',
