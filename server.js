2610c8f067c83625e15a95a3220b042cb4a9d08b
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-06-20 11:37:07 +0300

    completing authentication on the front-end

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ process.on('uncaughtException', (err) => {
   process.exit(1);
 });
 
-// const methodForTours = require('./dev-data/data/import-dev-data');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -20,7 +19,6 @@ const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`App running on ${PORT}`);
 });
-// methodForTours.importData();
 
 process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
