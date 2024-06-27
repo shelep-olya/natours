73b50566f33c279df30bb4b11242f47dac3be49e
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-06-27 12:50:49 +0300

    added updating-password page

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
 import { displayMap } from './mapbox';
-import { updateData } from './updateSettings';
+import { updateSettings } from './updateSettings';
 
 //DOM elements
 
@@ -10,6 +10,7 @@ const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 //Values
 
 //Delegation
@@ -32,6 +33,24 @@ if (userDataForm) {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const name = document.getElementById('name').value;
-    updateData(name, email);
+    updateSettings({ name, email }, 'data');
+  });
+}
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password',
+    );
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-confirm').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-current').value = '';
   });
 }
